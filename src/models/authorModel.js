e88c9559291e752e906a9c500407ed179af5cbd4
tushar199627 <tusharbarai6@gmail.com>
2022-06-21 15:10:53 +0530

    updated

---
+++ src/models/authorModel.js
@@ -0,0 +1,27 @@
+const mongoose =require('mongoose');
+
+const authorSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        enum: ["Mr", "Miss", "Mrs"]
+    },
+    firstName:{type:String,
+        required:true 
+    },
+    lastName:{ type: String,
+        required:true
+    },
+    emailId: {type:String,
+        unique:true
+
+    },
+    password: {type: String,
+        required:true
+    },
+  
+},
+    { timestamps: true });
+
+
+    module.exports = mongoose.model('author', authorSchema)
+
