39646ac8d30177e43e4996e42e047de7ea539ca2
tushar199627 <tusharbarai6@gmail.com>
2022-06-21 14:00:24 +0530

    updated

---
+++ src/models/blogModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const blogSchema = new mongoose.Schema( {
+    title:{ type:String,
+        required:true
+    },
+    body: {type:String,
+        required:true
+    },
+    authorId: {
+        type: ObjectId,
+        ref: "author",
+        required:true
+    },
+    tags: { type: [String],
+        required:true
+    },
+    subcategory: {
+        type: [String],
+    },
+    publishedAt: Date,
+    ispublished: {type: Boolean,
+                default:false
+    },
+    deleteAt: Date,
+    isDeleted:{type: Boolean,
+        default:false
+    },
+}, {timestamps: true });
+
+
+module.exports = mongoose.model('Blog', blogSchema) //users
+
+
